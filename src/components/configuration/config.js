2aa5b12e535091c43b78630f3edbdc362d7eb026
Martin Simeonov <i525452@fhict.nl>
2025-04-17 09:31:10 +0200

    Add netlify.toml fix

--- src/components/configuration/config.js
+++ src/components/configuration/config.js
@@ -1,27 +1,27 @@
 const API_BASE_URL =
     process.env.NODE_ENV === "development"
         ? "http://localhost:8080"
-        : "https://api.torque-hub.com/api";
+        : "/api";
 
 const WEBSOCKET_URL =
     process.env.NODE_ENV === "development"
         ? "ws://localhost:8080/ws"
-        : "wss://api.torque-hub.com/ws";
+        : "wss://torque-hub.com/ws";
 
 const ANNOUNCEMENT_WEBSOCKET_URL =
     process.env.NODE_ENV === "development"
             ? "ws://localhost:8081/ws"
-            : "wss://api.torque-hub.com/ws";
+            : "wss://tickets-api/ws";
 
 const API_BASE_URL_SHOWCASE =
 process.env.NODE_ENV === "development"
     ? "http://localhost:8082" // Showcase API
-    : "https://api.torque-hub.com/showcase-api"; // Adjust this for production
+    : "/showcase-api"; // Adjust this for production
 
 const API_BASE_URL_TICKETS =
 process.env.NODE_ENV === "development"
     ? "http://localhost:8081" // Showcase API
-    : "https://api.torque-hub.com/ticket-api"; // Adjust this for production
+    : "/ticket-api"; // Adjust this for production
 
 export { API_BASE_URL, WEBSOCKET_URL,
          API_BASE_URL_SHOWCASE,
