93f259a539d7234dc5c6189f2590f97f9dff9372
Martin Simeonov <i525452@fhict.nl>
2025-04-20 11:55:13 +0200

    config

--- src/components/configuration/config.js
+++ src/components/configuration/config.js
@@ -6,22 +6,22 @@ const API_BASE_URL =
 const WEBSOCKET_URL =
     process.env.NODE_ENV === "development"
         ? "ws://localhost:8080/ws"
-        : "wss://backend.torque-hub.com/ws";
+        : "ws://api/ws";
 
 const ANNOUNCEMENT_WEBSOCKET_URL =
     process.env.NODE_ENV === "development"
             ? "ws://localhost:8081/ws"
-            : "wss://backend.torque-hub.com/ticket-api/ws";
+            : "wss://ticket-api/ws";
 
 const API_BASE_URL_SHOWCASE =
 process.env.NODE_ENV === "development"
     ? "http://localhost:8082" // Showcase API
-    : "https://backend.torque-hub.com/showcase-api"; // Adjust this for production
+    : "/showcase-api"; // Adjust this for production
 
 const API_BASE_URL_TICKETS =
 process.env.NODE_ENV === "development"
     ? "http://localhost:8081" // Showcase API
-    : "https://backend.torque-hub.com/ticket-api"; // Adjust this for production
+    : "/ticket-api"; // Adjust this for production
 
 export { API_BASE_URL, WEBSOCKET_URL,
          API_BASE_URL_SHOWCASE,
