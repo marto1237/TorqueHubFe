5781d26acf4ad13f315a9c2011775b84db27a155
Martin Simeonov <i525452@fhict.nl>
2025-05-16 09:17:39 +0200

    Add fallback for bad gateway

--- src/components/configuration/config.js
+++ src/components/configuration/config.js
@@ -1,27 +1,27 @@
 const API_BASE_URL =
     process.env.NODE_ENV === "development"
         ? "http://localhost:8080"
-        : "/api";
+        : "https://backend.torque-hub.com/api";
 
 const WEBSOCKET_URL =
     process.env.NODE_ENV === "development"
         ? "ws://localhost:8080/ws"
-        : `${window.location.protocol === 'https:' ? 'wss:' : 'ws:'}//${window.location.host}/ws`;
+        : "wss://backend.torque-hub.com/ws";
 
 const ANNOUNCEMENT_WEBSOCKET_URL =
     process.env.NODE_ENV === "development"
             ? "ws://localhost:8081/ws"
-            : `${window.location.protocol === 'https:' ? 'wss:' : 'ws:'}//${window.location.host}/ticket-ws`;
+            : "wss://backend.torque-hub.com/ticket-ws";
 
 const API_BASE_URL_SHOWCASE =
 process.env.NODE_ENV === "development"
     ? "http://localhost:8082" // Showcase API
-    : "/showcase-api"; // Relative path for production
+    : "/showcase-api"; // Adjust this for production
 
 const API_BASE_URL_TICKETS =
 process.env.NODE_ENV === "development"
-    ? "http://localhost:8081" // Tickets API
-    : "/ticket-api"; // Relative path for production
+    ? "http://localhost:8081" // Showcase API
+    : "/ticket-api"; // Adjust this for production
 
 export { API_BASE_URL, WEBSOCKET_URL,
          API_BASE_URL_SHOWCASE,
